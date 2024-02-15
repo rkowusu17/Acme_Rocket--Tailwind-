6a7afeddb7ce826878e2ad0532ac7aa19d1e9d64
Reginald Owusu <reginaldmiles45@gmail.com>
2024-02-15 23:35:00 +0000

    small small

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ module.exports = {
   theme: {
     extend: {},
   },
+  screens: {
+    widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+    tallscreen: { raw: "(min-aspect-ratio: 13/20" },
+  },
+
   plugins: [],
 };
